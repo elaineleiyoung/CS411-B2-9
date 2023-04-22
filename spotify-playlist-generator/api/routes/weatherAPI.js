5a0b2c4bde0caa599c4502883a512b95bc27d3c1
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2023-04-22 19:15:15 -0400

    fixed backend api

--- spotify-playlist-generator/api/routes/weatherAPI.js
+++ spotify-playlist-generator/api/routes/weatherAPI.js
@@ -16,30 +16,27 @@ app.use(express.json());
 
 router.get("/", async (req, res) => {
     try {
-      const location = req.query.location;
-  
-      const coordinatesResponse = await fetch(
-        `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${OPENCAGE_API_KEY}`
-      );
-      try {
-        const coordinates = coordinatesData.results[0].geometry;
+        const location = req.query.location;
+        const coordinatesResponse = await fetch(
+          `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${OPENCAGE_API_KEY}`
+        );
+        const coordinatesData = await coordinatesResponse.json();
+      
+        try {
+          const coordinates = coordinatesData.results[0].geometry;
+          const weatherResponse = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lng}&units=metric&appid=${OPENWEATHER_API_KEY}`
+          );
+          const weatherData = await weatherResponse.json();
+          res.json({ location, coordinates, weather: weatherData });
+        } catch (error) {
+          console.error("Error fetching weather: ", error);
+          return res.status(500).send("Error fetching weather.");
+        }
       } catch (error) {
         console.error("Error fetching coordinates: ", error);
-        return res.status(500).send("Error fetching coordinates.");
-      }
-  
-      const weatherResponse = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lng}&units=metric&appid=${OPENWEATHER_API_KEY}`
-      );
-      const weatherData = await weatherResponse.json();
-      
-    //   console.log({ location, coordinates, weather: weatherData });
-      res.json({ location, coordinates, weather: weatherData });
-     
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Something went wrong" });
-    }
+        res.status(500).json({ error: "Something went wrong" });
+      }      
     //res.send("API WoRKS");
   });  
 
