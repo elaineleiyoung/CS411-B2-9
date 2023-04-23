61b7ce5e0dcd066494915366f6ffe9e5d0cb7a23
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2023-04-23 04:59:21 -0400

    working on spotify backend

--- spotify-playlist-generator/src/App.js
+++ spotify-playlist-generator/src/App.js
@@ -5,19 +5,36 @@ import Playlists from './Playlists';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
 import WeatherAPI from './WeatherAPI.js'
+import { accessToken, logout } from './spotify';
+
+import Button from '@mui/material/Button';
 
 
 function App() {
-  
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spotify Playlist Generator</h1>
         <WeatherAPI />
+        {!token ? (
+          <Button variant="contained" href="http://localhost:9000/spotify/login">
+            Log in to Spotify
+          </Button>
+        ) : (
+          <>
+            <h1>Logged in!</h1>
+            <Button variant="contained" onClick={logout}>Log Out</Button>
+          </>
+        )}
         {/* <Weather /> */}
-        <Login />
-        <Playlists />
+        {/* <Login />
+        <Playlists /> */}
       </header>
     </div>
   );
