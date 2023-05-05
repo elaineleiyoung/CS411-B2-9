57494a4ca2dd050eafdbb5c69cc3487e268c078d
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2023-05-05 11:55:21 -0400

    documentation

--- spotify-playlist-generator/api/routes/spotify.js
+++ spotify-playlist-generator/api/routes/spotify.js
@@ -1,5 +1,7 @@
+// Load environment variables from .env file
 require('dotenv').config();
 
+// Import required modules
 const express = require('express');
 const axios = require('axios');
 const querystring = require('querystring');
@@ -10,24 +12,23 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-
 const router = express.Router();
 
+// generateRandomString: generates a random string of specified length
 const generateRandomString = length => {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  };
-  
-  
-  const stateKey = 'spotify_auth_state';
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
 
+const stateKey = 'spotify_auth_state';
+
+// Login route: generates an authorization URL and redirects the user to it
 router.get('/login', function(req, res) {
-
-    const state = generateRandomString(16);
+  const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   const scope = 'user-read-private user-read-email';
@@ -42,77 +43,80 @@ router.get('/login', function(req, res) {
   });
 
   res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
-  });
-  
+});
 
+// Callback route: handles the response from Spotify after authorization
 router.get('/callback', (req, res) => {
-    const code = req.query.code || null;
-    const error = req.query.error;
-    if (error){
-      console.log('callback error')
-      res.redirect('http://localhost:3000');
-    }
+  const code = req.query.code || null;
+  const error = req.query.error;
+  if (error){
+    console.log('callback error')
+    res.redirect('http://localhost:3000');
+  }
   else{
-  axios({
-    method: 'post',
-    url: 'https://accounts.spotify.com/api/token',
-    data: querystring.stringify({
-      grant_type: 'authorization_code',
-      code: code,
-      redirect_uri: REDIRECT_URI
-    }),
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
-    },
-  })
-  .then(response => {
-    if (response.status === 200) {
-      const { access_token, refresh_token, expires_in } = response.data;
-  
-      const queryParams = querystring.stringify({
-        access_token,
-        refresh_token,
-        expires_in,
-      });
-  
-      res.redirect(`http://localhost:3000/?${queryParams}`);
-  
-    } else {
-      res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
-    }
-  })
-    .catch(error => {
-      res.send(error);
-    });
-}});
-
-  router.get('/refresh_token', (req, res) => {
-    const { refresh_token } = req.query;
-  
+    // Request access and refresh tokens from Spotify
     axios({
       method: 'post',
       url: 'https://accounts.spotify.com/api/token',
       data: querystring.stringify({
-        grant_type: 'refresh_token',
-        refresh_token: refresh_token
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
       }),
       headers: {
         'content-type': 'application/x-www-form-urlencoded',
         Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
       },
     })
-      .then(response => {
-        res.send(response.data);
-      })
+    .then(response => {
+      if (response.status === 200) {
+        const { access_token, refresh_token, expires_in } = response.data;
+
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+          expires_in,
+        });
+
+        res.redirect(`http://localhost:3000/?${queryParams}`);
+
+      } else {
+        res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
+      }
+    })
       .catch(error => {
         res.send(error);
       });
-  });
-  
+  }
+});
+
+// Refresh token route: refreshes access token using the refresh token
+router.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 module.exports = router;
 
+// Recommendations route: fetches song recommendations based on genre and acousticness
 router.get("/recommendations", (req, res) => {
   console.log("Received request");
   const access_token = req.headers.authorization.split(" ")[1];
@@ -120,6 +124,7 @@ router.get("/recommendations", (req, res) => {
   const acousticness = req.query.acousticness;
   const limit = req.query.limit;
 
+  // Make a request to the Spotify API for recommendations
   axios({
     method: "get",
     url: `https://api.spotify.com/v1/recommendations?seed_genres=${genre}&target_acousticness=${acousticness}&limit=${limit}`,
@@ -137,22 +142,23 @@ router.get("/recommendations", (req, res) => {
     });
 });
 
+// Profile route: fetches user's profile information from the Spotify API
+router.get("/profile", async (req, res) => {
+  const accessToken = req.headers.authorization.split(" ")[1];
 
+  try {
+    // Make a request to the Spotify API for user's profile information
+    const response = await axios.get("https://api.spotify.com/v1/me", {
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch profile data from Spotify API" });
+  }
+});
 
-  router.get("/profile", async (req, res) => {
-    const accessToken = req.headers.authorization.split(" ")[1];
-
-    try {
-      const response = await axios.get("https://api.spotify.com/v1/me", {
-        headers: {
-          "Authorization": `Bearer ${accessToken}`,
-        },
-      });
-      res.json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Failed to fetch profile data from Spotify API" });
-    }
-  });
 
 
--- spotify-playlist-generator/api/routes/weatherAPI.js
+++ spotify-playlist-generator/api/routes/weatherAPI.js
@@ -1,19 +1,19 @@
+// Import required modules and packages
 require('dotenv').config();
 var express = require("express");
 
-// const cors = require("cors");
 const fetch = require("node-fetch");
 
 const app = express();
 var router = express.Router();
-// const PORT = process.env.PORT || 9000;
 
+// Access environment variables
 const OPENCAGE_API_KEY = process.env.OPENCAGE_API_KEY;
 const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
 
-// app.use(cors());
 app.use(express.json());
 
+// Define a route for fetching weather information based on the location
 router.get("/", async (req, res) => {
   try {
     const location = req.query.location;
@@ -21,6 +21,7 @@ router.get("/", async (req, res) => {
       return res.status(400).json({ error: "Location not specified" });
     }
 
+    // Fetch coordinates for the given location from OpenCage Geocoding API
     const coordinatesResponse = await fetch(
       `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${OPENCAGE_API_KEY}`
     );
@@ -28,10 +29,14 @@ router.get("/", async (req, res) => {
 
     try {
       const coordinates = coordinatesData.results[0].geometry;
+
+      // Fetch weather data for the given coordinates from OpenWeatherMap API
       const weatherResponse = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lng}&units=metric&appid=${OPENWEATHER_API_KEY}`
       );
       const weatherData = await weatherResponse.json();
+      
+      // Return location, coordinates, and weather information
       res.json({ location, coordinates, weather: weatherData });
     } catch (error) {
       console.error("Error fetching weather: ", error);
@@ -41,9 +46,7 @@ router.get("/", async (req, res) => {
     console.error("Error fetching coordinates: ", error);
     res.status(500).json({ error: "Something went wrong" });
   }
-  //res.send("API WoRKS");
 });
 
 // Use the router
 module.exports = router;
-
